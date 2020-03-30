d69ab798c57e877a864562f8df5f72b89ddb80d5
Hernan Manrique <hernan.pk92@hotmail.com>
2020-03-30 10:39:58 -0500

    Se ajusta el formulario de registro, para que haga validaciones

--- loginApp/src/app/pages/registro/registro.component.ts
+++ loginApp/src/app/pages/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: UsuarioModel;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+   }
 
+   onSubmit(formRegister: NgForm) {
+     if (formRegister.invalid) {
+       return;
+     }
+     console.log('Formulario enviado');
+     console.log(formRegister);
+     
+     console.log(this.usuario);
+   }
 
 }
