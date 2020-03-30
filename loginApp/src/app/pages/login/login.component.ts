fcb8f7280bece873ee40b69ac2a60a5ec0a27be2
Hernan Manrique <hernan.pk92@hotmail.com>
2020-03-30 15:49:21 -0500

    Por autenticación y registro, se redirecciona al Home

--- loginApp/src/app/pages/login/login.component.ts
+++ loginApp/src/app/pages/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -11,7 +15,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel;
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -21,11 +25,24 @@ export class LoginComponent implements OnInit {
     if (form.invalid) {
       return; 
     }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.auth.signIn(this.usuario)
               .subscribe( (response) => {
-                console.log(response);
+                Swal.close();
+                this.router.navigateByUrl('/home');
               }, (errorResponse) => {
-                console.error(errorResponse.error.error.message);
+                Swal.fire({
+                  icon: 'error',
+                  title: 'Error al autenticar',
+                  text: 'Ocurrio un error inesperado'
+                });
               });
   }
 
--- loginApp/src/app/pages/registro/registro.component.ts
+++ loginApp/src/app/pages/registro/registro.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-registro',
@@ -12,7 +17,7 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor(private auth:AuthService) { }
+  constructor(private auth:AuthService, private router: Router) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -22,11 +27,24 @@ export class RegistroComponent implements OnInit {
      if (formRegister.invalid) {
        return;
      }
+
+     Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
      this.auth.signUp(this.usuario)
               .subscribe( (response) => {
-                console.log(response);
+                Swal.close();
+                this.router.navigateByUrl('/home');
               }, (errorResponse) => {
-                console.error(errorResponse.error.error.message);
+                Swal.fire({
+                  icon: 'error',
+                  title: 'Error al autenticar',
+                  text: 'Ocurrio un error inesperado'
+                });
               });
    }
 
